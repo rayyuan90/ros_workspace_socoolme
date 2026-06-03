e8da0d0be3f8e9974111194c07aa148fb290c1ac
rayyuan <rayyuan90@gmail.com>
2026-06-03 14:39:29 +0800

    add multi parameters to node

---
+++ src/python_parameter_event_handler/python_parameter_event_handler/parameter_event_handler.py
@@ -0,0 +1,51 @@
+import rclpy
+from rclpy.node import Node
+import rclpy.parameter
+
+from rclpy.parameter_event_handler import ParameterEventHandler
+
+
+class SampleNodeWithParameters(Node):
+    def __init__(self):
+        super().__init__('node_with_parameters')
+
+        self.declare_parameter('an_int_param', 0)
+        self.declare_parameter("another_double_param", 0.0)
+        # Create the parameter event handler for this node
+        self.handler = ParameterEventHandler(self)
+
+        # Register an event callback for parameter events
+        self.event_callback_handle = self.handler.add_parameter_event_callback(
+            callback=self.event_callback,
+        )
+
+        # Register parameter-specific callbacks
+        self.callback_handle = self.handler.add_parameter_callback(
+            parameter_name="an_int_param",
+            node_name="node_with_parameters",
+            callback=self.callback,
+        )
+
+        self.callback_handle2 = self.handler.add_parameter_callback(
+            parameter_name="a_double_param",
+            node_name="parameter_blackboard",
+            callback=self.callback,
+        )
+
+    def callback(self, p: rclpy.parameter.Parameter) -> None:
+        self.get_logger().info(f"Received an update to parameter: {p.name}: {rclpy.parameter.parameter_value_to_python(p.value)}")
+
+    def event_callback(self, parameter_event):
+        self.get_logger().info(f"Received parameter event from node {parameter_event.node}")
+
+        for p in parameter_event.changed_parameters:
+            self.get_logger().info(
+                f"Inside event: {p.name} changed to: {rclpy.parameter.parameter_value_to_python(p.value)}"
+            )
+
+
+def main():
+    rclpy.init()
+    node = SampleNodeWithParameters()
+    rclpy.spin(node)
+    rclpy.shutdown()
